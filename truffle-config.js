fb5a9944712b31d155dad6c00df8de0de2e5585d
yeosiowvic <victoryeo1974@gmail.com>
2021-11-18 14:39:40 +0800

    add kovan testnet to truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -38,8 +38,19 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+    kovan: {
+      provider: function(){
+        return new HDWalletProvider(
+          privateKeys.split(','),
+          'https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}'
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
     },
   },
 
